5f1d2ffa47dcea583006d2ada22f831494aae84c
priyankakatoch <priyankakatoch623@gmail.com>
2025-07-26 19:56:54 +0530

    Time: 4 ms (16.00%) | Memory: 17.8 MB (76.27%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -5,13 +5,11 @@ class Solution:
         for i in range(0,n-1):
             if nums[i]==nums[i+1]:
                 nums[i],nums[i+1] = nums[i]*2,nums[i]*0
-        r0=[]
-        for i in nums:
-            if i!=0:
-                result.append(i)
-            else:
-                r0.append(i)
-        print(result,r0)
-        return result+r0
+        for nums in nums:
+            if nums != 0:
+                result.append(nums)
+        while len(result)<n:
+            result.append(0)
+        return result
 
         
