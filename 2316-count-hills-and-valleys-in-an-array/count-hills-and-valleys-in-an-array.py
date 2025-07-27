81994313b2295d276f746e6bf1eb68bc59f96005
priyankakatoch <priyankakatoch623@gmail.com>
2025-07-28 01:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (64.44%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -1,32 +1,12 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        res = 0  # number of peaks and valleys
         n = len(nums)
-        for i in range(1, n - 1):
-            if nums[i] == nums[i - 1]:
-                # deduplication
+        count = 0
+        p = nums[0]
+        for i in range (1,n-1):
+            if nums[i] == nums[i+1]:
                 continue
-            left = (
-                0  # left side possibly unequal neighboring corresponding state
-            )
-            for j in range(i - 1, -1, -1):
-                if nums[j] > nums[i]:
-                    left = 1
-                    break
-                elif nums[j] < nums[i]:
-                    left = -1
-                    break
-            right = (
-                0  # right side possibly unequal neighboring corresponding state
-            )
-            for j in range(i + 1, n):
-                if nums[j] > nums[i]:
-                    right = 1
-                    break
-                elif nums[j] < nums[i]:
-                    right = -1
-                    break
-            if left == right and left != 0:
-                # at this time, index i is part of a peak or valley.
-                res += 1
-        return res
+            if (p>nums[i]<nums[i+1]) or (p<nums[i]>nums[i+1]):
+                count+=1 
+            p=nums[i]
+        return count
