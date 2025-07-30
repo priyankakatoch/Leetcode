42b9cca06dd0cfa3589f648a70455154b6d034e2
priyankakatoch <priyankakatoch623@gmail.com>
2025-07-30 10:55:59 +0530

    Time: 67 ms (25.59%) | Memory: 42.4 MB (42.16%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -3,12 +3,12 @@ class Solution:
         n=len(nums)
         pos = []
         neg = []
-        for i in range (0,n):
-            if nums[i]>0:
+        for i in range(0,n):
+            if nums[i] >0:
                 pos.append(nums[i])
             else :
                 neg.append(nums[i])
-        for i in range (0,len(pos)):
-            nums[2*i] = pos[i]
-            nums[2*i+1] = neg[i]
+        for i in range(0,len(pos)):
+            nums[i*2] = pos[i]
+            nums[i*2+1] = neg[i]
         return nums        
